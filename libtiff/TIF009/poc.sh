22876d497555a7cd696ec1861c26417cdec2bc1b
Srividya Subramanian <ssubramanian@ethz.ch>
2025-05-20 22:19:15 +0200

    Add POC descriptions

--- libtiff/TIF009/poc.sh
+++ libtiff/TIF009/poc.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 
+# https://www.cve.org/CVERecord?id=CVE-2019-7663
+
 docker run --rm \
     $TAG \
     tiffcp /test/crash /test/crash_out
 
+# Segmentation fault: exit code 139 (SIGSEGV)
 echo "Exit code: $?"
