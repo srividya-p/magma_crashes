da00d70e8e9e199c1eb5326541dca265667ea06a
Srividya Subramanian <ssubramanian@ethz.ch>
2025-05-20 22:20:01 +0200

    Add PoC for TIF007

---
+++ libtiff/TIF007/poc.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# https://www.cve.org/CVERecord?id=CVE-2016-10270
+
+docker run -it --rm \
+    $TAG \
+    tiffinfo -D /test/crash
+
+# OOM Error: Memory allocation attempt 4449586117620 over memory limit (268435456)
+echo "Exit code: $?"
